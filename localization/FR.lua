397b1967964dd255e8c124fb2743d57a22b241e2
silvereyes <silvereyes333@gmail.com>
2017-07-16 17:56:53 -0500

    Version 2.2.1
    - Update 15 Horns of the Reach compatibility
    - French translation files; thanks @lexo1000
    - Update LibAddonMenu to r24

--- localization/FR.lua
+++ localization/FR.lua
@@ -1,7 +1,9 @@
 local strings = {
     ["SI_CBE_AND"]                                             = " et ",
-    ["SI_CBE_DISABLE_GUILDBANK_WITHDRAWAL_AUTO_STASH"]         = "Retrait de banque de guilde sans sac d'artisanat",
-    ["SI_CBE_DISABLE_GUILDBANK_WITHDRAWAL_AUTO_STASH_TOOLTIP"] = "Lorsqu'elle est activée , le retrait des matériaux d'artisanat à partir d'une banque de guilde gardera les matériaux dans votre sac d'inventaire . Ils ne seront pas transférés automatiquement à votre ESO+ sac de artisanat ."
+    ["SI_CBE_DISABLE_GUILDBANK_WITHDRAWAL_AUTO_STASH"]         = "Désactiver le transfert auto vers le sac d'artisanat",
+    ["SI_CBE_DISABLE_GUILDBANK_WITHDRAWAL_AUTO_STASH_TOOLTIP"] = "Les matériaux d'artisanat retirés à partir d'une banque iront dans le sac d'inventaire. Ils ne seront pas transférés automatiquement vers le sac d'artisanat ESO+.",
+    ["SI_CBE_PRIMARY_ACTIONS_USE_DEFAULT"]                     = "Affecter le stockage/retrait rapide",
+    ["SI_CBE_PRIMARY_ACTIONS_USE_DEFAULT_TOOLTIP"]             = "La case à cocher par défaut de de la boîte de dialogue de stockage et de retrait affecte également les actions principales de stockage et de retrait. Si cette option est désactivée, les actions de retrait et de stockage utiliseront toujours la quantité maximale.",
 }
 
 -- Overwrite English strings
